9af3213c1f1e648210282ee42cbb880d33f8f1cc
ernestchu <shchu@citi.sinica.edu.tw>
2022-11-23 10:12:50 +0000

    update

--- hubconf.py
+++ hubconf.py
@@ -4,6 +4,7 @@ import torch
 
 def DCE_net(scale_factor=1):
     _DCE_net = enhance_net_nopool(scale_factor)
-    _DCE_net.load_state_dict(torch.load('zero_dcepp/weights/Epoch99.pth'))
+    checkpoint = 'To-be-added'
+    _DCE_net.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint))
 
     return _DCE_net
