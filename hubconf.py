f90b31ebdc845d00077ad3acca6a7961ead3e10a
ernestchu <shchu@citi.sinica.edu.tw>
2022-11-23 11:45:52 +0000

    add custom device

--- hubconf.py
+++ hubconf.py
@@ -2,8 +2,8 @@ dependencies = ['torch']
 from zero_dcepp import enhance_net_nopool
 import torch
 
-def DCE_net(scale_factor=1):
-    _DCE_net = enhance_net_nopool(scale_factor)
+def DCE_net(scale_factor=1, device='cuda'):
+    _DCE_net = enhance_net_nopool(scale_factor).to(device)
     checkpoint = 'https://github.com/ernestchu/Zero-DCE_extension/releases/download/0.0.1/Epoch99.pth'
     _DCE_net.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint))
 
