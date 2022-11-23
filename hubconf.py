2db53ac1dcf6f7119147380d88e2eac15e7d79f7
ernestchu <shchu@citi.sinica.edu.tw>
2022-11-23 10:05:49 +0000

    fix bug

--- hubconf.py
+++ hubconf.py
@@ -2,7 +2,7 @@ dependencies = ['torch']
 from zero_dcepp import enhance_net_nopool
 import torch
 
-def DCE_net(scale_factor):
+def DCE_net(scale_factor=1):
     _DCE_net = enhance_net_nopool(scale_factor)
     _DCE_net.load_state_dict(torch.load('zero_dcepp/weights/Epoch99.pth'))
 
