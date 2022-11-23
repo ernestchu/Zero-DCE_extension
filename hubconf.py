023ab4869f9c628aafb661f30e045354d1f4379b
ernestchu <shchu@citi.sinica.edu.tw>
2022-11-23 10:15:12 +0000

    fix ckpt link

--- hubconf.py
+++ hubconf.py
@@ -4,7 +4,7 @@ import torch
 
 def DCE_net(scale_factor=1):
     _DCE_net = enhance_net_nopool(scale_factor)
-    checkpoint = 'To-be-added'
+    checkpoint = 'https://github.com/ernestchu/Zero-DCE_extension/releases/download/0.0.1/Epoch99.pth'
     _DCE_net.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint))
 
     return _DCE_net
